6d6f399f087f52b2321d67653fb53e059b549c1c
Matthew <MatthewGebara@gmail.com>
2022-10-02 22:18:10 -0400

    Finalized code.

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,7 @@ var number = "1,2,3,4,5,6,7,8,9,0";
 var trueNumber = number.split(","); //after writing it out the first time i realized I'm lazy and this is an easier way to make arrays
 
 // Write password to the #password input
-function writePassword() {
+function writePassword() { //this can all be left as is.
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
@@ -23,19 +23,19 @@ function writePassword() {
 
 }
 function generatePassword(){
-  choice = [];
+  var choice = [];
   length = parseInt(prompt("How long do you want your password to be (Number of charactesr)")) //figure out length of password
   if (!length){
     alert("This needs a value");
   }
-  else if (length < 8 || length > 128) {
+  else if (length < 8 || length > 128) { //if the length is within 8-128 then it will go forward.
     length = parseInt(prompt("You must choose between 8 and 128"));
   }
   else {
     //gather requirements for password
     if (confirm("Will this contain Numbers?")){
     choice = choice.concat(trueNumber);} //it took me like 10 minutes to realize i had to have curly brackets not parenthesis.
-    if (confirm("Will this contain Special Characters?")){
+    if (confirm("Will this contain Special Characters?")){ //each one makes the array for choice bigger and include different character types.
     choice = choice.concat(trueSpecialCharacter);
     }
     if (confirm("Will there be Uppercase Letters?")){
@@ -45,11 +45,12 @@ function generatePassword(){
       choice = choice.concat(lowerCase);
     }
   };
-  for(var i = 0; i <= length; i++) {
-    var randomIndex = Math.floor(Math.random() * trueNumber.length);
+  let password = ''; //without this i get an HTML error and the password doesn't reset when you generate a new one.
+  for(var i = 0; i <= length; i++) { //loop that generates the password using the index created from the different arrays
+    var randomIndex = Math.floor(Math.random() * choice.length);
     password = password + choice[randomIndex];
   }
-  return password;
+  return password; //now when the function is called it will give back the password
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Homework-3
+A simple password generator made using Javascript. The user simply has to click on "Generate Password" and answer the prompts they'd like to use to give it features.
